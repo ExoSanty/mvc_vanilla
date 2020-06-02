cf6f16af2b13ea147337f6119d61d04c68eeec7d
Siddhartha Gupta <32353198+sidmustdie@users.noreply.github.com>
2020-06-03 00:51:40 +0530

    Added Views

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// App Root
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+
+define('URLROOT', 'http://localhost/mvc_vanilla');
+
+// Site Name
+
+define('SITENAME', 'MVC-Vanilla');
